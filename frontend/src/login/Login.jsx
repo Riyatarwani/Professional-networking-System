3f7beb2b203127c44f665850ce47ab0b1f8b1986
Riya Tarwani <96372000+Riyatarwani@users.noreply.github.com>
2025-12-28 00:23:48 +0530

    Import api utility in Login component

--- frontend/src/login/Login.jsx
+++ frontend/src/login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import axios from 'axios';
+import api from "../utils/api";
 import { toast } from 'react-toastify';
 
 const Login = () => {
